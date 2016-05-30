bb73eb00ac449972fe6478dd134beaacdae683bf
Leonel Quinteros <leonel.quinteros@gmail.com>
2016-05-30 16:25:56 -0300

    go fmt

--- tpl/tpl.go
+++ tpl/tpl.go
@@ -1,43 +1,43 @@
 package tpl
 
 import (
-    "io/ioutil"
-    "sync"
+	"io/ioutil"
+	"sync"
 )
 
-// Tpl centralizes all package base functionality into a single type designed to be composited by others. 
+// Tpl centralizes all package base functionality into a single type designed to be composited by others.
 type Tpl struct {
-    // Custom template path from where to render
-    TplPath string
-    
-    // Custom template name used to build the complete path
-    TplName string
-    
-    // cache
-    cache map[string]string
-    
-    // Sync Mutex
+	// Custom template path from where to render
+	TplPath string
+
+	// Custom template name used to build the complete path
+	TplName string
+
+	// cache
+	cache map[string]string
+
+	// Sync Mutex
 	sync.RWMutex
 }
 
-// Cached reads the content of the path and returns the file's content or returns an error when fails. 
+// Cached reads the content of the path and returns the file's content or returns an error when fails.
 // When successful, it saves the content on the cache and next time reads from there.
 func (t *Tpl) Cached(path string) (string, error) {
-    t.Lock()
+	t.Lock()
 	defer t.Unlock()
-	
+
 	if t.cache == nil {
-	    t.cache = make(map[string]string)
+		t.cache = make(map[string]string)
 	}
-	
-    if _, ok := t.cache[path]; !ok {
-        data, err := ioutil.ReadFile(path)
-        if err != nil {
-            return "", err
-        }
-        
-        t.cache[path] = string(data)
-    }
-    
-    return t.cache[path], nil
+
+	if _, ok := t.cache[path]; !ok {
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			return "", err
+		}
+
+		t.cache[path] = string(data)
+	}
+
+	return t.cache[path], nil
 }
