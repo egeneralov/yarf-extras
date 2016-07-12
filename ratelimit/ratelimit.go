31ca5dd57b1ed83b1104582fa1e9994a23a11bdc
Leonel Quinteros <leonel.quinteros@gmail.com>
2016-07-12 17:15:02 -0300

    Rate limit module

---
+++ ratelimit/ratelimit.go
@@ -0,0 +1,160 @@
+package ratelimit
+
+import (
+    "time"
+    "sync"
+)
+
+// RateLimitError indicates that the limit has been reached for the actual window.
+type RateLimitError struct{}
+
+func (e RateLimitError) Error() string {
+	return "Rate limit exceeded"
+}
+
+// Rate represents a single client count for a given event.
+// Its used internally by RateLimit to count different keys.
+type Rate struct {
+    // Amount of events allowed in a given window
+    Limit int64
+    
+    // Time window in seconds to limit
+    Window int64
+    
+    // Actual count
+    EventCount int64
+    
+    // Start time
+    Start time.Time
+    
+    // Sync Mutex
+    sync.RWMutex
+}
+
+// Count checks for the actual limit/window and resets the window (Start) and Count when corresponding. 
+// If the limit has been reached for the actual window it returns a RateLimitError error. 
+func (r *Rate) Count() error {
+    r.Lock()
+    defer r.Unlock()
+    
+    // Reset window
+    if time.Now().After( r.Start.Add(time.Second * time.Duration(r.Window)) ) {
+        r.Start = time.Now()
+        r.EventCount = 0
+    }
+    
+    // Block
+    if r.EventCount >= r.Limit {
+        return RateLimitError{}
+    }
+    
+    // Count
+    r.EventCount++
+    
+    // Continue
+    return nil
+}
+
+type RateLimit struct {
+    // Amount of events allowed in a given window
+    Limit int64
+    
+    // Time window in seconds to limit
+    Window int64
+    
+    // Count storage
+    counter map[string]*Rate
+    
+    // Sync Mutex
+    sync.RWMutex
+    
+    // Garbage collection flag
+    gcActive bool
+}
+
+func New(limit, window int64) *RateLimit {
+    return &RateLimit{
+        Limit: limit,
+        Window: window,
+        counter: make(map[string]*Rate),
+        gcActive: false,
+    }
+}
+
+func (rl *RateLimit) Get(key string) *Rate {
+    rl.RLock()
+    defer rl.RUnlock()
+    
+    if _, ok := rl.counter[key]; !ok {
+        // Default new Rate
+        return &Rate{
+            Limit: rl.Limit,
+            Window: rl.Window,
+            EventCount: 0,
+            Start: time.Now(),
+        }
+    }
+    
+    return rl.counter[key]
+}
+
+// Count checks on a given key, for the actual limit/window and resets the window (Start) and Count when corresponding. 
+// If the limit has been reached for the actual window it returns a RateLimitError error.
+func (rl *RateLimit) Count(key string) error {
+    rl.Lock()
+    defer rl.Unlock()
+    
+    // Init counter
+    if rl.counter == nil {
+        rl.counter = make(map[string]*Rate)
+    }
+    
+    // Init garbage collector
+    if !rl.gcActive {
+        rl.gcActive = true
+        go rl.gc()
+    }
+    
+    // Init rate count for the given key
+    if _, ok := rl.counter[key]; !ok {
+        rl.counter[key] = &Rate{
+            Limit: rl.Limit,
+            Window: rl.Window,
+            EventCount: 0,
+            Start: time.Now(),  
+        }
+    }
+    
+    return rl.counter[key].Count()
+}
+
+func (rl *RateLimit) gc() {
+    // 10 times the window + 1
+    t := time.NewTicker( time.Duration((rl.Window + 1) * 10) * time.Second)
+
+	for _ = range t.C {
+		// Cancel when storage not present
+		if rl.counter == nil {
+			t.Stop()
+			rl.gcActive = false
+			return
+		}
+
+		// Check for expired entries.
+		now := time.Now()
+
+        // Read lock
+		rl.RLock()
+		defer rl.RUnlock()
+		
+		for key, rate := range rl.counter {
+		    // Expired 2 windows ago.
+		    if now.After( rate.Start.Add(time.Second * time.Duration(rate.Window * 2)) ) {
+			    // Write lock
+			    rl.Lock()
+				delete(rl.counter, key)
+				rl.Unlock()
+			}
+		}
+	}
+}
---
+++ ratelimit/yarf_middleware.go
@@ -0,0 +1,73 @@
+package ratelimit
+
+import (
+	"github.com/yarf-framework/yarf"
+	"strconv"
+	"time"
+)
+
+// YarfError is the custom error type compatible with Yarf's YError
+type YarfError struct{}
+
+// Implements the error interface returning the ErrorMsg value of each error.
+func (e *YarfError) Error() string {
+	return "Unauthorized"
+}
+
+// Code returns the error's HTTP code to be used in the response.
+func (e *YarfError) Code() int {
+	return 429 // Too Many Requests
+}
+
+// ID returns the error's ID for further reference.
+func (e *YarfError) ID() int {
+	return 429
+}
+
+// Msg returns the error's message, used to implement the Error interface.
+func (e *YarfError) Msg() string {
+	return "Too Many Requests"
+}
+
+// Body returns the error's content body, if needed, to be returned in the HTTP response.
+func (e *YarfError) Body() string {
+	return "Too Many Requests: Try again later."
+}
+
+
+// RateLimiter middleware provides request rate limits per IP
+type RateLimiter struct {
+	yarf.Middleware
+	
+	// rate limiter
+	rl *RateLimit
+}
+
+func YarfMiddleware(limit, window int64) *RateLimiter {
+    return &RateLimiter {
+        rl: New(limit, window),
+    }
+}
+
+func (m *RateLimiter) PreDispatch(c *yarf.Context) error {
+    // IP as key
+    key := c.GetClientIP()
+    
+    // Count
+    err := m.rl.Count(key)
+    if err != nil {
+        if _, ok := err.(RateLimitError); ok {
+            return &YarfError{}
+        } 
+         
+        return err
+    }
+    
+    // Set rate limit info on headers
+    rate := m.rl.Get(key)
+    c.Response.Header().Set("X-RateLimit-Limit", strconv.Itoa(int(rate.Limit)))
+    c.Response.Header().Set("X-RateLimit-Remaining", strconv.Itoa(int(rate.Limit - rate.EventCount)))
+    c.Response.Header().Set("X-RateLimit-Reset", strconv.Itoa(int(rate.Start.Add(time.Second * time.Duration(rate.Window)).Unix())))
+    
+	return nil
+}
